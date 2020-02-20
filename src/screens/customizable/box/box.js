ca466dc4f7f2bff779409278cfa363b877c0fcd9
Bruno Moura <bruno8moura@gmail.com>
2020-02-20 05:06:41 -0300

    Image fits within whole div.

--- src/screens/customizable/box/box.js
+++ src/screens/customizable/box/box.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './box.css';
+import supermarioimg from './img/Super_Mario_World.jpg'
 
 const borderRadiousRegulator = ({className, id}) => {
     let properties = {
@@ -14,15 +15,25 @@ const borderRadiousRegulator = ({className, id}) => {
     return( React.createElement("input", properties, null));
 }
 
+const img = ({src, className}) => {
+    let properties = {
+        src,
+        className
+    };
+
+    return( React.createElement("img", properties, null) );
+}
+
 const customizableBox = (props) => {
-    let inputs = [
+    let elements = [
                     borderRadiousRegulator({className: "topLeftRegulator", id:"topLeftRegulator"}),
                     borderRadiousRegulator({className: "topRightRegulator", id:"topRightRegulator"}),
                     borderRadiousRegulator({className: "bottomLeftRegulator", id:"bottomLeftRegulator"}),
-                    borderRadiousRegulator({className: "bottomRightRegulator", id:"bottomRightRegulator"})
+                    borderRadiousRegulator({className: "bottomRightRegulator", id:"bottomRightRegulator"}),
+                    img({src: supermarioimg, className: "imagebackground"})
                 ];
                 
-    return( React.createElement("div", {className: "box"}, inputs) );
+    return( React.createElement("div", {className: "box"}, elements) );
 }
 
 export default customizableBox;
